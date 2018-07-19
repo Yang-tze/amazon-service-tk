a3bfa90cddc57f42c77cb1424f7b4d8f31536b4d
Tony <tony.kijak@gmail.com>
2018-07-19 14:56:37 -0700

    configures eslint

--- client/components/ShippingInfo.jsx
+++ client/components/ShippingInfo.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 class ShippingInfo extends React.Component {
   constructor(props) {
-  	super(props);
-  	this.state = {
-  	};
+    super(props);
+    this.state = {
+    };
   }
 
   render() {
-  	return (
-  	  <div>Hello World x2</div>
-  	);
+    return (
+      <div>
+      Hello World x2
+      </div>
+    );
   }
 }
 
--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import ProductInfo from './components/ProductInfo.jsx';
-import ShippingInfo from './components/ShippingInfo.jsx';
+import ProductInfo from './components/ProductInfo';
+import ShippingInfo from './components/ShippingInfo';
 
 ReactDOM.render(<ProductInfo />, document.getElementById('product-info'));
 ReactDOM.render(<ShippingInfo />, document.getElementById('shipping-info'));
