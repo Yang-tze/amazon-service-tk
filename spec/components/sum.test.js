443b2cd83fa891840eb2c8f64b153b8998d66f46
Tony <tony.kijak@gmail.com>
2018-07-19 18:29:35 -0700

    implements jest with enzyme

--- spec/components/sum.test.js
+++ spec/components/sum.test.js
@@ -1,4 +1,7 @@
-const sum = require('./sum');
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
+import sum from './sum.js';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
