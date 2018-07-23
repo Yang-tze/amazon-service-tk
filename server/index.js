86da26e18a1dc9898bcb3ac0c98b56de54cf76d9
Tony <tony.kijak@gmail.com>
2018-07-23 16:02:31 -0700

    uses path to serve static files

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('../public'));
+app.use(express.static(path.join(path.dirname(__dirname), 'public')));
 app.use(bodyParser.json());
 
 app.get('/products/:id', (req, res) => {
