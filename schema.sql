45a01d32426a5d7c1f65556402351fce4c292e52
Tony <tony.kijak@gmail.com>
2018-07-23 13:09:49 -0700

    inserts one row of sample data

--- schema.sql
+++ schema.sql
@@ -2,11 +2,13 @@ CREATE DATABASE product_db;
 
 USE product_db;
 
+-- DROP TABLE related_products, products;
+
 CREATE TABLE products (
 id INTEGER PRIMARY KEY,
 brand VARCHAR(100) NOT NULL,
 name VARCHAR(200) NOT NULL,
-product_tier JSON NOT NULL,
+product_tier VARCHAR(50) NOT NULL,
 product_options JSON NOT NULL,
 price JSON NOT NULL,
 about_product JSON NOT NULL,
@@ -33,6 +35,4 @@ FOREIGN KEY (product_id) REFERENCES products(id),
 FOREIGN KEY (related_product_id) REFERENCES products(id)
 );
 
-INSERT INTO products (id, brand, name, product_options, price, about_product, 
-  is_prime, stock_count, reviews, stars, questions, seller, thumbnail) VALUES();
-
+INSERT INTO products (id, brand, name, product_tier, product_options, price, about_product, is_prime, stock_count, reviews, stars, questions, seller, thumbnail) VALUES(1, 'Jockey', 'Kyra', 'Elite', '{"color":["green","white","blue","black","silver","purple"],"size":["S","M","L","XL"]}', '{"msrp":110.98,"list":99.88,"sale":99.88}', '[" Four dollar toast velit anim, ex cronut quis brooklyn hot chicken"," Umami coloring book enim, post-ironic aliquip taxidermy neutra adipisicing mixtape cupidatat glossier"," Distillery pinterest butcher farm-to-table, iceland synth brunch la croix"]', 0, 72, 359, 1, 48, 'Amazon', 'BioFueled.jpg');
