83561598a5d7264d422250e7b970b16c7b09b1f0
Tony <tony.kijak@gmail.com>
2018-07-25 17:16:46 -0700

    adds sample data

---
+++ client/components/mockData.js
@@ -0,0 +1,27 @@
+module.exports = {
+  id: 1,
+  brand: 'Jockey',
+  name: 'Kyra',
+  product_tier: 'gluten-free',
+  product_options: {
+    color: ['green', 'white', 'blue', 'black', 'silver', 'purple'],
+    size: ['S', 'M', 'L', 'XL'],
+  },
+  price: {
+    msrp: 110.98,
+    list: 99.88,
+    sale: 99.88,
+  },
+  about_product: [
+    'Four dollar toast velit anim, ex cronut quis brooklyn hot chicken',
+    'Umami coloring book enim, post-ironic aliquip taxidermy neutra adipisicing mixtape cupidatat glossier',
+    'Distillery pinterest butcher farm-to-table, iceland synth brunch la croix',
+  ],
+  is_prime: 1,
+  stock_count: 72,
+  reviews: 359,
+  stars: 4,
+  questions: 48,
+  seller: 'Aamazon',
+  thumbnail: 'gluten-free.jpg',
+};
