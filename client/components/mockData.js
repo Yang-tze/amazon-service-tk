035a68b480179e6eba8b517e56ad10af725f6060
Tony <tony.kijak@gmail.com>
2018-07-25 19:14:04 -0700

    adds related product information

--- client/components/mockData.js
+++ client/components/mockData.js
@@ -1,27 +1,39 @@
 module.exports = {
-  id: 1,
-  brand: 'Jockey',
-  name: 'Kyra',
-  productTier: 'Gluten-Free',
-  productOptions: {
-    color: ['green', 'white', 'blue', 'black', 'silver', 'purple'],
-    size: ['S', 'M', 'L', 'XL'],
+  data: [{
+    id: 1,
+    brand: 'Jockey',
+    name: 'Kyra',
+    productTier: 'Gluten-Free',
+    productOptions: {
+      color: ['green', 'white', 'blue', 'black', 'silver', 'purple'],
+      size: ['S', 'M', 'L', 'XL'],
+    },
+    price: {
+      msrp: 110.98,
+      list: 99.88,
+      sale: 99.88,
+    },
+    aboutProduct: [
+      'Four dollar toast velit anim, ex cronut quis brooklyn hot chicken',
+      'Umami coloring book enim, post-ironic aliquip taxidermy neutra adipisicing mixtape cupidatat glossier',
+      'Distillery pinterest butcher farm-to-table, iceland synth brunch la croix',
+    ],
+    isPrime: 0,
+    stockCount: 72,
+    reviews: 359,
+    stars: 4,
+    questions: 48,
+    seller: 'Aamazon',
+    thumbnail: 'gluten-free.jpg',
   },
-  price: {
-    msrp: 110.98,
-    list: 99.88,
-    sale: 99.88,
-  },
-  aboutProduct: [
-    'Four dollar toast velit anim, ex cronut quis brooklyn hot chicken',
-    'Umami coloring book enim, post-ironic aliquip taxidermy neutra adipisicing mixtape cupidatat glossier',
-    'Distillery pinterest butcher farm-to-table, iceland synth brunch la croix',
-  ],
-  isPrime: 0,
-  stockCount: 72,
-  reviews: 359,
-  stars: 4,
-  questions: 48,
-  seller: 'Aamazon',
-  thumbnail: 'gluten-free.jpg',
+  {
+    id: 4,
+    price: {
+      msrp: 119.98,
+      list: 89.98,
+      sale: 89.98,
+    },
+    productTier: 'Turbo',
+    thumbnail: 'Turbo.jpg',
+  }],
 };
