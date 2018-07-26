8d5fd4f1ef8a78d94f12acae4bcb19e76b3bd9c0
Tony <tony.kijak@gmail.com>
2018-07-26 14:16:28 -0700

    adds basic component state info

--- client/components/productInfoComponents/ItemColors.jsx
+++ client/components/productInfoComponents/ItemColors.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const ItemColors = (props) => {
+  const { tier } = props;
+  const { related } = props;
   return (
-    <div>
-      This is the item colors component
+    <div className="colors">
+      { related.length > 0 ? <img src={`${tier}.jpg`} alt="product" /> : <span></span> }
+      { related.map(el => <img src={el.thumbnail} alt="related product" key={el.id} />) }
     </div>
   );
 };
