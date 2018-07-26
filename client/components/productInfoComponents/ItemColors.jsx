9491a2923d88bf9195ce9d12534c3fe3c672ada5
Tony <tony.kijak@gmail.com>
2018-07-26 11:02:21 -0700

    adds sizing and color components

---
+++ client/components/productInfoComponents/ItemColors.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemColors = (props) => {
+  return (
+    <div>
+      This is the item colors component
+    </div>
+  );
+};
+
+export default ItemColors;
---
+++ client/components/productInfoComponents/ItemSizing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemSizing = (props) => {
+  return (
+    <div>
+      These are the sizing options
+    </div>
+  );
+};
+
+export default ItemSizing;
