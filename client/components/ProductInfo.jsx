8f76018a1b7e560d72643a939f12faa920921081
Tony <tony.kijak@gmail.com>
2018-07-25 17:17:24 -0700

    adds fetch function, imports sample data

--- client/components/ProductInfo.jsx
+++ client/components/ProductInfo.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Data from './mockData.js';
 
 class ProductInfo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      productId: Data,
     };
+  }
+
+  get() {
+    fetch('http://127.0.0.1:3003/products/1')
+      .then(response => response.json())
+      .then(obj => console.log(obj))
+      .catch(err => console.error(err));
   }
 
   render() {
