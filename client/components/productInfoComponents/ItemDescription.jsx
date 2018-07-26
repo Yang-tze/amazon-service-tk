272e6d5f4ef4947521e9537ff8290350974127b9
Tony <tony.kijak@gmail.com>
2018-07-26 11:04:36 -0700

    adds item description component

---
+++ client/components/productInfoComponents/ItemDescription.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemDescription = (props) => {
+  return (
+    <div>
+      This is the item&#39s description
+    </div>
+  );
+};
+
+export default ItemDescription;
