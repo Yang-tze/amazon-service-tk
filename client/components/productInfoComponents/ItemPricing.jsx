ad5d14ca24a6bd72895c07f6e623ba4b6b718b59
Tony <tony.kijak@gmail.com>
2018-07-26 12:19:54 -0700

    adds pricing info to component

--- client/components/productInfoComponents/ItemPricing.jsx
+++ client/components/productInfoComponents/ItemPricing.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const ItemPricing = (props) => {
+  const { price } = props;
+  const { isPrime } = props;
   return (
     <div>
-      This is the reviews
+      <span>
+        Price:
+      </span>
+      <span>
+        {`$${price.sale}`}
+      </span>
+      <span>
+        &
+      </span>
+      <a href="">
+        Free Returns
+      </a>
     </div>
   );
 };
