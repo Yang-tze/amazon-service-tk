f1542ec472f556624ca5eec2f9b143e84c3b45a4
Tony <tony.kijak@gmail.com>
2018-07-25 18:37:16 -0700

    updates key names to camelCase

--- client/components/mockData.js
+++ client/components/mockData.js
@@ -2,8 +2,8 @@ module.exports = {
   id: 1,
   brand: 'Jockey',
   name: 'Kyra',
-  product_tier: 'gluten-free',
-  product_options: {
+  productTier: 'Gluten-Free',
+  productOptions: {
     color: ['green', 'white', 'blue', 'black', 'silver', 'purple'],
     size: ['S', 'M', 'L', 'XL'],
   },
@@ -12,13 +12,13 @@ module.exports = {
     list: 99.88,
     sale: 99.88,
   },
-  about_product: [
+  aboutProduct: [
     'Four dollar toast velit anim, ex cronut quis brooklyn hot chicken',
     'Umami coloring book enim, post-ironic aliquip taxidermy neutra adipisicing mixtape cupidatat glossier',
     'Distillery pinterest butcher farm-to-table, iceland synth brunch la croix',
   ],
-  is_prime: 1,
-  stock_count: 72,
+  isPrime: 0,
+  stockCount: 72,
   reviews: 359,
   stars: 4,
   questions: 48,
