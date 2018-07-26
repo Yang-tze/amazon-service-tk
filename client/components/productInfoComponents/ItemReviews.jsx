f1bc89399c007282a4df9fe973ec70bb2c3ea883
Tony <tony.kijak@gmail.com>
2018-07-26 16:51:10 -0700

    adds equation to calc stars, adds spacing between stars, reviews

--- client/components/productInfoComponents/ItemReviews.jsx
+++ client/components/productInfoComponents/ItemReviews.jsx
@@ -2,12 +2,38 @@ import React from 'react';
 
 const ItemReviews = (props) => {
   const { reviewInfo } = props;
-  const { reviews, stars, questions } = reviewInfo;
+  const { reviews, questions } = reviewInfo;
+  const reviewCount = reviews.reduce((acc, val) => acc + val, 0);
+  const weighted = reviews.reduce((acc, val, idx) => acc + (val * parseInt(idx, 10)), 0);
+  const stars = Math.round(weighted * 10 / reviewCount) / 10 + 1;
   return (
     <div>
-      <span>Stars: {stars} </span>
-      <span>Reviews: {reviews} </span>
-      <span>Questions: {questions} </span>
+      <span>
+        {stars}
+        &nbsp;
+      </span>
+      <img src="" alt="stars" />
+      <a href="">
+        <span>
+          {reviewCount}
+          &nbsp;
+        </span>
+        <span>
+          customer reviews
+        </span>
+      </a>
+      <span>
+        &nbsp;|&nbsp;
+      </span>
+      <a href="">
+        <span>
+          {questions}
+          &nbsp;
+        </span>
+        <span>
+          answered questions
+        </span>
+      </a>
     </div>
   );
 };
