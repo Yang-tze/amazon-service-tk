7026e17273f02ab1e74afa2508546040422b8084
Tony <tony.kijak@gmail.com>
2018-07-26 13:26:12 -0700

    adds basic state info component

--- client/components/productInfoComponents/ItemSizing.jsx
+++ client/components/productInfoComponents/ItemSizing.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const ItemSizing = (props) => {
+  const { sizing } = props;
   return (
     <div>
-      These are the sizing options
+      <h4>
+        Sizing:
+      </h4>
+      <div>
+        <select>
+          <option />
+          {sizing.size.map(el => <option key={el}>{el}</option>)}
+        </select>
+        <a href="">
+          Size Chart
+        </a>
+      </div>
     </div>
   );
 };
