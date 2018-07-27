10c2e33310c69e6aa6b3228fa645a0d69f350fa3
Tony <tony.kijak@gmail.com>
2018-07-27 10:30:04 -0700

    adds class names for css styling

--- client/components/productInfoComponents/ItemReviews.jsx
+++ client/components/productInfoComponents/ItemReviews.jsx
@@ -7,33 +7,27 @@ const ItemReviews = (props) => {
   const weighted = reviews.reduce((acc, val, idx) => acc + (val * parseInt(idx, 10)), 0);
   const stars = Math.round(weighted * 10 / reviewCount) / 10 + 1;
   return (
-    <div>
-      <span>
-        {stars}
-        &nbsp;
-      </span>
-      <img src="" alt="stars" />
-      <a href="">
-        <span>
-          {reviewCount}
+    <div className="reviews-module">
+      <span className="reviews-star-meter">
+        <div className="reviews-star-wrapper">
+          {stars}
           &nbsp;
-        </span>
-        <span>
-          customer reviews
-        </span>
-      </a>
+          <img src="" alt="stars" />
+        </div>
+      </span>
+      <span className="reviews-review-count">
+        <a href="">
+          {`${reviewCount} customer reviews`}
+        </a>
+      </span>
       <span>
         &nbsp;|&nbsp;
       </span>
-      <a href="">
-        <span>
-          {questions}
-          &nbsp;
-        </span>
-        <span>
-          answered questions
-        </span>
-      </a>
+      <span className="reviews-question-count">
+        <a href="">
+          {`${questions} answered questions`}
+        </a>
+      </span>
     </div>
   );
 };
