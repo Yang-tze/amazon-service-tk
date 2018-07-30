4923efd8bf542b2ac7b924803e297f69760f145d
Tony <tony.kijak@gmail.com>
2018-07-30 15:51:52 -0700

    adds response object from db query

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const database = require('./database.js');
 
 const app = express();
 
@@ -8,7 +9,7 @@ app.use(express.static(path.join(path.dirname(__dirname), 'public')));
 app.use(bodyParser.json());
 
 app.get('/products/:id', (req, res) => {
-  res.send();
+  database.getAll(req.params.id, results => res.send(results));
 });
 
 app.listen(3003, () => console.log('Listening on port 3003'));
