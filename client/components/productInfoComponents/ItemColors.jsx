5fc1fc1c25d6b66eeb0e50c9cf8dfe1c03a9aba4
Tony <tony.kijak@gmail.com>
2018-07-31 12:32:43 -0700

    refractors for readability

--- client/components/productInfoComponents/ItemColors.jsx
+++ client/components/productInfoComponents/ItemColors.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import styles from '../../style/productInfoComponents/ItemColors.css';
 
+const ListItemOne = (props) => {
+  const { tier, thumbnail, handlers } = props;
+  const { onMouseEnter, onMouseLeave, onSelect } = handlers;
+
+  return (
+    <li>
+      <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onSelect} data-id={null} data-tier={tier}>
+        <img src={`https://s3-us-west-1.amazonaws.com/viamis/${thumbnail}`} alt={tier} />
+      </div>
+    </li>
+  );
+};
+
+const ListItems = (props) => {
+  const { object, handlers } = props;
+  const { onMouseEnter, onMouseLeave, onSelect } = handlers;
+
+  return (
+    <li key={object.id}>
+      <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onSelect} data-id={`/${object.id}/`} data-tier={object.product_tier}>
+        <img src={`https://s3-us-west-1.amazonaws.com/viamis/${object.thumbnail}`} alt={object.thumbnail} />
+      </div>
+    </li>
+  );
+};
+
 const ItemColors = (props) => {
   const { tier, related, handlers } = props;
   const { thumbnail } = props;
   const { onMouseEnter, onMouseLeave, onSelect } = handlers;
+
   return (
     <div>
       <div>
@@ -17,20 +44,9 @@ const ItemColors = (props) => {
       </div>
       <ul className={styles.list}>
         { related.length > 0
-          ? (
-            <li>
-              <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onSelect} data-id={null} data-tier={tier}>
-                <img src={`https://s3-us-west-1.amazonaws.com/viamis/${thumbnail}`} alt={tier} />
-              </div>
-            </li>
-          ) : <span /> }
-        { related.map(el => (
-          <li key={el.id}>
-            <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onSelect} data-id={`/${el.id}/`} data-tier={el.product_tier}>
-              <img src={`https://s3-us-west-1.amazonaws.com/viamis/${el.thumbnail}`} alt={el.thumbnail}/>
-            </div>
-          </li>
-        )) }
+          ? <ListItemOne handlers={handlers} thumbnail={thumbnail} tier={tier} />
+          : <span /> }
+        { related.map((el, idx) => <ListItems handlers={handlers} object={el} key={`item${idx + 1}`} />) }
       </ul>
     </div>
   );
