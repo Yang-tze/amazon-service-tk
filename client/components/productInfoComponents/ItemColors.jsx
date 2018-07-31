b6cf032b1a6f261722bc99ead8d806b5c8ca27e8
Tony <tony.kijak@gmail.com>
2018-07-31 08:46:52 -0700

    adds event handler functions to node elements

--- client/components/productInfoComponents/ItemColors.jsx
+++ client/components/productInfoComponents/ItemColors.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import styles from '../../style/productInfoComponents/ItemColors.css';
 
 const ItemColors = (props) => {
-  const { tier } = props;
-  const { related } = props;
+  const { tier, related, handlers } = props;
+  const { thumbnail } = props;
+  const { onMouseEnter, onMouseLeave, onSelect } = handlers;
   return (
     <div>
       <div>
@@ -15,8 +16,21 @@ const ItemColors = (props) => {
         </span>
       </div>
       <ul className={styles.list}>
-        { related.length > 0 ? <li><div><img src={`https://s3-us-west-1.amazonaws.com/viamis/${tier}.jpg`} alt={tier} /></div></li> : <span /> }
-        { related.map(el => <li><div><img src={`https://s3-us-west-1.amazonaws.com/viamis/${el.thumbnail}`} alt={el.thumbnail} key={el.id} /></div></li>) }
+        { related.length > 0
+          ? (
+            <li>
+              <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onSelect} data-id={null} data-tier={tier}>
+                <img src={`https://s3-us-west-1.amazonaws.com/viamis/${thumbnail}`} alt={tier} />
+              </div>
+            </li>
+          ) : <span /> }
+        { related.map(el => (
+          <li key={el.id}>
+            <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onSelect} data-id={`/${el.id}/`} data-tier={el.product_tier}>
+              <img src={`https://s3-us-west-1.amazonaws.com/viamis/${el.thumbnail}`} alt={el.thumbnail}/>
+            </div>
+          </li>
+        )) }
       </ul>
     </div>
   );
