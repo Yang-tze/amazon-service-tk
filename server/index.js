e8af4966d61e83cefb5b82819bab47bb7cd0a1ae
Tony <tony.kijak@gmail.com>
2018-07-30 20:29:03 -0700

    adds routing for url with product id

--- server/index.js
+++ server/index.js
@@ -5,11 +5,21 @@ const database = require('./database.js');
 
 const app = express();
 
-app.use(express.static(path.join(path.dirname(__dirname), 'public')));
 app.use(bodyParser.json());
 
-app.get('/products/:id', (req, res) => {
-  database.getAll(req.params.id, results => res.send(results));
+app.get('*/bundle.js', (req, res) => {
+  res.sendFile(path.join(path.dirname(__dirname), 'public/bundle.js'));
 });
+
+
+app.get('/products/:id', (req, res) => {
+  if (parseInt(req.params.id, 10)) {
+    database.getAll(req.params.id, results => res.send(results));
+  } else {
+    res.end();
+  }
+});
+
+app.use('/*', express.static(path.join(path.dirname(__dirname), 'public')));
 
 app.listen(3003, () => console.log('Listening on port 3003'));
