b601fca7ed1d979cb5417f15fc60f2a481270d8e
Tony <tony.kijak@gmail.com>
2018-08-03 20:33:09 -0700

    adds syling to dropdown list

--- client/components/productInfoComponents/ItemSizing.jsx
+++ client/components/productInfoComponents/ItemSizing.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import styles from '../../style/productInfoComponents/ItemSizing.css';
 
 const ItemSizing = (props) => {
   const { sizing, onClick } = props;
   return (
     <div>
-      <h4>
+      <h4 className={styles.title}>
         Size:
       </h4>
-      <div className="">
-        <select>
-          <option />
-          {sizing.size.map(el => <option key={el}>{el}</option>)}
+      <div className={styles.container}>
+        <span className={styles.label}>Select</span>
+        <select className={styles.select}>
+          <option className={styles.option} value="" disabled selected />
+          {sizing.size.map(el => <option className={styles.option} key={el}>{el}</option>)}
         </select>
-        <a href="" onClick={onClick}>
+        <a className={styles.link} href="" onClick={onClick}>
           Size Chart
         </a>
       </div>
