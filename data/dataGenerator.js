7f22469667580952145f7b137c3da4f0be7d961d
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-07 18:24:38 -0700

    Begin optimizing data generation

--- data/dataGenerator.js
+++ data/dataGenerator.js
@@ -1,10 +1,15 @@
 const faker = require('faker');
+const zlib = require('zlib');
 const fs = require('fs');
+
+const gzip = zlib.createGzip();
 
 const raw = require('./rawData.js');
 
-const nameRoot = 'name';
-const productCount = 100;
+const startTime = new Date();
+
+const nameRoot = 'hamazon';
+const productCount = 1000000;
 const productHeadings = 'id\tname\tbrand\tproduct_tier\tproduct_options\tprice\tabout_product\tis_prime\tstock_count\treviews\tquestions\tseller\tthumbnail';
 
 const imageCount = 3;
@@ -14,22 +19,20 @@ const randomInt = (min, max) => min + Math.floor(Math.random() * (max - min));
 
 const randomNum = (min, max) => min + Math.random() * (max - min);
 
-const generateOptions = () => JSON.stringify({ color: raw.color, size: raw.size });
+const tab = str => `${str}\t`;
+
+const generateOptions = () => `{"size":${raw.size}}`;
 
 const generatePrice = () => {
   const msrp = randomInt(10, 400) - 0.01;
-  const list = Math.floor(randomNum(0.8, 0.9) * msrp) - 0.01;
-  const sale = Math.floor(randomNum(0.8, 0.9) * list) - 0.01;
-  return JSON.stringify({ msrp, list, sale });
+  // const list = Math.floor(randomNum(0.8, 0.9) * msrp) - 0.01;
+  // const sale = Math.floor(randomNum(0.8, 0.9) * list) - 0.01;
+  return `{"msrp":${msrp}}`; // , "list": ${list}, "sale": ${sale} }`;
 };
 
 const generateAbout = () => {
-  const about = [
-    raw.sentences[randomInt(0, raw.sentences.length)],
-    raw.sentences[randomInt(0, raw.sentences.length)],
-    raw.sentences[randomInt(0, raw.sentences.length)],
-  ];
-  return JSON.stringify(about);
+  const about = `["${raw.sentences[randomInt(0, raw.sentences.length)].trim()}","${raw.sentences[randomInt(0, raw.sentences.length)].trim()}","${raw.sentences[randomInt(0, raw.sentences.length)].trim()}"]`;
+  return about;
 };
 
 const generateReviews = (rawScore) => {
@@ -37,60 +40,54 @@ const generateReviews = (rawScore) => {
     const weight = Math.abs(index / 5 - rawScore);
     randomInt(0, 1000 * weight);
   });
-  return JSON.stringify(reviews);
-};
-
-const writeProducts = (productCount, stream) => {
-  let productList = productHeadings;
-  for (let i = 1; i <= productCount; i++) {
-    productList += `${generateProduct(i)}\n`;
-  }
-  return productList;
+  return `${reviews}`;
 };
 
 const generateProduct = (id) => {
-  const name = nameRoot + id;
-  const brand = faker.company.companyName();
-  const options = generateOptions();
-  const price = generatePrice();
-  const about = generateAbout();
-  const tier = faker.company.catchPhraseAdjective();
-  const prime = faker.random.boolean();
-  const stock = randomInt(10, 200);
-  const reviews = generateReviews(Math.random());
-  const questions = randomInt(3, 50);
-  const seller = faker.name.firstName();
-  const thumbnail = `${imageEndpoint}${id % imageCount}.png`;
-  return [
-    id,
-    name,
-    brand,
-    options,
-    price,
-    about,
-    tier,
-    prime,
-    stock,
-    reviews,
-    questions,
-    seller,
-    thumbnail,
-  ].join('\t');
+  const productId = tab(`${id}`);
+  const name = tab(nameRoot + id);
+  const brand = tab(faker.name.lastName());
+  const options = tab(generateOptions());
+  const price = tab(generatePrice());
+  const about = tab(generateAbout());
+  const tier = tab(faker.company.catchPhraseAdjective());
+  const prime = tab(faker.random.boolean());
+  const stock = tab(randomInt(10, 200));
+  const reviews = tab(generateReviews(Math.random()));
+  const questions = tab(randomInt(3, 50));
+  const seller = tab(faker.name.firstName());
+  const thumbnail = tab(`${imageEndpoint}${id % imageCount}.png`);
+  return (
+    productId
+    + name
+    + brand
+    + options
+    + price
+    + about
+    + tier
+    + prime
+    + stock
+    + reviews
+    + questions
+    + seller
+    + thumbnail
+  );
 };
 
-const handleResponse = (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('success!');
+const writeProducts = (productCount, stream) => {
+  stream.write(productHeadings);
+  for (let i = 1; i <= productCount; i++) {
+    stream.write(`${generateProduct(i)}\n`);
+    if (i % 100000 === 0) {
+      console.log(i, new Date() - startTime);
+    }
   }
+  console.log(new Date() - startTime);
+  stream.end();
 };
 
-fs.writeFile(`${__dirname}/products.tsv`, writeProducts(productCount), 'utf8', handleResponse);
+const out = fs.createWriteStream(`${__dirname}/products.tsv`, { highWaterMark: Math.pow(2, 32) });
 
-// const stream = fs.createWriteStream('./data/products.tsv', {flags:'a'});
-// console.log(new Date().toISOString());
-// [...Array(10000)].forEach( function (item,index) {
-//     stream.write(index + "\n");
-// });
-// stream.end();
+gzip.pipe(out);
+
+writeProducts(productCount, out);
