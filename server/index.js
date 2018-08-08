c32e184cd88d6e918cc99efe39f6be6f89a17a6c
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-08 15:23:44 -0700

    Streamline static file serving

--- server/index.js
+++ server/index.js
@@ -4,16 +4,18 @@ const path = require('path');
 const cors = require('cors');
 const router = require('./router.js');
 
+const port = process.env.PORT || 3003;
+
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
-app.use('/*', express.static(path.join(path.dirname(__dirname), 'public')));
 
-app.get('*/bundle.js', (req, res) => {
-  res.sendFile(path.join(path.dirname(__dirname), 'public/bundle.js'));
-});
+// app.get('/:id', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
+app.use('/:id', express.static(path.join(__dirname, '../public')));
 
 app.listen(3003, () => console.log('Listening on port 3003'));
