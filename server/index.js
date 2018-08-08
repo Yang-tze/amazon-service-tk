8a7b28c97fa46071d8ff104399a1031451cd53ce
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-07 18:26:21 -0700

    Set up skeletal requests handlers for post/put/delete operations

--- server/index.js
+++ server/index.js
@@ -22,6 +22,30 @@ app.get('/products/:id', (req, res) => {
   }
 });
 
+app.post('/products/:id', (req, res) => {
+  // if (parseInt(req.params.id, 10)) {
+  //   database.getAll(req.params.id, results => res.send(results));
+  // } else {
+  //   res.end();
+  // }
+});
+
+app.put('/products/:id', (req, res) => {
+  // if (parseInt(req.params.id, 10)) {
+  //   database.getAll(req.params.id, results => res.send(results));
+  // } else {
+  //   res.end();
+  // }
+});
+
+app.delete('/products/:id', (req, res) => {
+  // if (parseInt(req.params.id, 10)) {
+  //   database.getAll(req.params.id, results => res.send(results));
+  // } else {
+  //   res.end();
+  // }
+});
+
 app.use('/*', express.static(path.join(path.dirname(__dirname), 'public')));
 
 app.listen(3003, () => console.log('Listening on port 3003'));
