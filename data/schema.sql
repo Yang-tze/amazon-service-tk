5ba873cd508832d83d493c351174a1b12f9aab4b
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-08 16:28:14 -0700

    MReorganize db folder and modify schema.sql to use with postgres

---
+++ data/schema.sql
@@ -0,0 +1,26 @@
+-- Run as: psql -f schema.sql product_db
+
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS related_products;
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(200) NOT NULL,
+  brand VARCHAR(100) NOT NULL,
+  tier VARCHAR(50) NOT NULL,
+  options JSON NOT NULL,
+  price JSON NOT NULL,
+  about JSON NOT NULL,
+  is_prime BOOLEAN NOT NULL,
+  stock_count INTEGER NOT NULL,
+  reviews JSON NOT NULL,
+  questions INTEGER NOT NULL,
+  seller VARCHAR(150) NOT NULL,
+  thumbnail TEXT NOT NULL
+);
+
+CREATE TABLE related_products (
+  id SERIAL PRIMARY KEY,
+  product_id INTEGER NOT NULL,
+  related_id INTEGER NOT NULL
+);
