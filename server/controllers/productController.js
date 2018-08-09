9a7282a65023b5af4fa0eef7750c94dca3b14257
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-09 14:24:37 -0700

    Clarify controller file names

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -8,19 +8,25 @@ module.exports = {
       res.end();
     }
   },
-  getById(req, res) {
-    if (parseInt(req.params.id, 10)) {
-      model.getAll(req.params.id, results => res.send(results));
+  getProductById(req, res) {
+    const {
+      params: { id },
+    } = req;
+    if (parseInt(id, 10)) {
+      model.getProductById(id, results => res.send(results));
     } else {
       res.end();
     }
   },
-  getByName(req, res) {
-    // if (parseInt(req.params.id, 10)) {
-    //   model.getAll(req.params.id, results => res.send(results));
-    // } else {
-    //   res.end();
-    // }
+  getProductByName(req, res) {
+    const {
+      params: { name },
+    } = req;
+    if (name) {
+      model.getProductByName(name, results => res.send(results));
+    } else {
+      res.end();
+    }
   },
   patchProduct(req, res) {
     // if (parseInt(req.params.id, 10)) {
