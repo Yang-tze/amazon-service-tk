c33019e8233b3a69362aa13a7b4688fccaf5c8a8
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-09 12:21:49 -0700

    Be more specific about what 'tune' means for postgres db

--- data/postgres-fk.sql
+++ data/postgres-fk.sql
@@ -1,5 +1,2 @@
-ALTER TABLE product_reviews ADD CONSTRAINT fk_review FOREIGN KEY (product_id) REFERENCES products(id);
-
 ALTER TABLE about_product ADD CONSTRAINT fk_about FOREIGN KEY (product_id) REFERENCES products(id);
-
 ALTER TABLE related_products ADD CONSTRAINT fk_related FOREIGN KEY (product_id) REFERENCES products(id);
