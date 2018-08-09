bbeef72ba9fbe400eb25c487252eade1b7217805
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-09 14:31:23 -0700

    Write first new API request handlers in model.js

--- server/model.js
+++ server/model.js
@@ -1,49 +1,48 @@
-const mysql = require('mysql');
+const connection = require('../data/postgres.js');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'product_db',
-});
-
-const parseData = function parseStringifiedObjectsFromDataQuery(object) {
-  const keys = ['about_product', 'price', 'reviews', 'product_options'];
-  keys.forEach((key) => {
-    if (object[key]) {
-      object[key] = JSON.parse(object[key]);
-    }
-  });
-  return object;
+const handleResults = (err, results, callback) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  callback(results);
 };
 
-const getProduct = function getProductInformation(productId, callback) {
-  connection.query(`select * from products where id=${productId}`, (err, results) => {
-    if (err) console.error(err);
-    callback(results);
+const getProductById = (productId, callback) => {
+  const queryString = `SELECT * FROM products WHERE id=${productId}`;
+  connection.query(queryString, (err, results) => {
+    handleResults(err, results, callback);
   });
 };
 
-const getRelated = function getRelatedProducts(productName, productId, callback) {
-  connection.query(
-    `select id, product_tier, price, stock_count, thumbnail from products where name='${productName}' and id <> ${productId}`,
-    (err, results) => {
-      if (err) console.error(err);
-      callback(results);
-    },
-  );
-};
-
-const getAll = function getProductAndRelatedProducts(productId, callback) {
-  const storage = {};
-  getProduct(productId, (results) => {
-    storage.data = parseData(results[0]);
-    const productName = results[0].name;
-    getRelated(productName, productId, (relatedResults) => {
-      storage.related = relatedResults.map(object => parseData(object));
-      callback(storage);
-    });
+const getProductByName = (productName, callback) => {
+  const queryString = `SELECT * FROM products WHERE product_name='${productName}'`;
+  connection.query(queryString, (err, results) => {
+    handleResults(err, results, callback);
   });
 };
+
+// const getRelated = function getRelatedProducts(productName, productId, callback) {
+//   connection.query(
+//     `select id, product_tier, price, stock_count, thumbnail from products where name='${productName}' and id <> ${productId}`,
+//     (err, results) => {
+//       if (err) console.error(err);
+//       callback(results);
+//     },
+//   );
+// };
+
+// const getAll = function getProductAndRelatedProducts(productId, callback) {
+//   const storage = {};
+//   getProduct(productId, (results) => {
+//     storage.data = parseData(results[0]);
+//     const productName = results[0].name;
+//     getRelated(productName, productId, (relatedResults) => {
+//       storage.related = relatedResults.map(object => parseData(object));
+//       callback(storage);
+//     });
+//   });
+// };
 
 // NEW crud functions
 
@@ -92,8 +91,8 @@ const deleteProduct = (productId, callback) => {
 };
 
 module.exports = {
-  getProduct,
-  getRelated,
+  getProductById,
+  getProductByName,
   getAll,
   addProduct,
   updateProduct,
