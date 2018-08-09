59161dc22abcc160a85eb246187f580678f5f1e9
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-09 14:53:11 -0700

    Modify postgres tuning script to add indices for product_ids

--- data/postgres-tune.sql
+++ data/postgres-tune.sql
@@ -1,2 +1,7 @@
+CREATE INDEX product_name ON products (product_name);
+
+CREATE INDEX product_id ON about_product (product_id);
 ALTER TABLE about_product ADD CONSTRAINT fk_about FOREIGN KEY (product_id) REFERENCES products(id);
+
+CREATE INDEX product_id ON related_products (product_name);
 ALTER TABLE related_products ADD CONSTRAINT fk_related FOREIGN KEY (product_id) REFERENCES products(id);
