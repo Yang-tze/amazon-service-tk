a07ff88e8f69ddb4d3210f0433e2f8ee80a8a0c4
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-08 22:13:55 -0700

    Add tables for reviews and about data

--- data/postgres-schema.sql
+++ data/postgres-schema.sql
@@ -10,8 +10,24 @@ CREATE TABLE products (
   is_prime BOOLEAN NOT NULL,
   stock_count INTEGER NOT NULL,
   questions INTEGER NOT NULL,
-  seller VARCHAR(150) NOT NULL,
+  seller VARCHAR(50) NOT NULL,
   thumbnail TEXT NOT NULL
+);
+
+CREATE TABLE reviews (
+  id SERIAL,
+  product_id INTEGER NOT NULL,
+  one_star INTEGER NOT NULL,
+  two_star INTEGER NOT NULL,
+  three_star INTEGER NOT NULL,
+  four_star INTEGER NOT NULL,
+  five_star INTEGER NOT NULL
+);
+
+CREATE TABLE about (
+  id SERIAL,
+  product_id INTEGER NOT NULL,
+  info VARCHAR(150) NOT NULL,
 );
 
 CREATE TABLE related_products (
