a519d4334947de1e7a87b7c579f2fb277ba4d8a0
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-09 16:53:43 -0700

    Remove unnecessary commented-out lines

--- data/postgres.js
+++ data/postgres.js
@@ -1,9 +1,6 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  // host: 'database.server.com',
-  // user: 'dbuser',
-  // password: '',
   database: 'product_db',
   port: 5432,
 });
