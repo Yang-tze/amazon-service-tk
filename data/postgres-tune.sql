f2cb39b8c1a5ae142d60478639094b11ff369151
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-09 10:25:56 -0700

    Add script for creating foreign key constraints in postgres

--- data/postgres-tune.sql
+++ data/postgres-tune.sql
@@ -1,5 +1,5 @@
-ALTER TABLE product_reviews ADD CONSTRAINT distfk (product_id) REFERENCES products (id) MATCH SIMPLE;
+ALTER TABLE product_reviews ADD CONSTRAINT fk_review FOREIGN KEY (product_id) REFERENCES products(id);
 
-ALTER TABLE about_product ADD CONSTRAINT distfk (product_id) REFERENCES products (id) MATCH SIMPLE;
+ALTER TABLE about_product ADD CONSTRAINT fk_about FOREIGN KEY (product_id) REFERENCES products(id);
 
-ALTER TABLE related_products ADD CONSTRAINT distfk (product_id) REFERENCES products (id) MATCH SIMPLE;
+ALTER TABLE related_products ADD CONSTRAINT fk_related FOREIGN KEY (product_id) REFERENCES products(id);
