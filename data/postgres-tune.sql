77a55d883902ae06aa46f3b168c1a33a06ff1629
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-09 15:01:54 -0700

    Fix typo

--- data/postgres-tune.sql
+++ data/postgres-tune.sql
@@ -3,5 +3,5 @@ CREATE INDEX product_name ON products (product_name);
 CREATE INDEX product_id ON about_product (product_id);
 ALTER TABLE about_product ADD CONSTRAINT fk_about FOREIGN KEY (product_id) REFERENCES products(id);
 
-CREATE INDEX product_id ON related_products (product_name);
+CREATE INDEX product_id ON related_products (product_id);
 ALTER TABLE related_products ADD CONSTRAINT fk_related FOREIGN KEY (product_id) REFERENCES products(id);
