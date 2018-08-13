9a0fdf4cc2b6dffb8741cfd3c0d79eb0d6df2ec6
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-13 14:55:47 -0700

    Implement server-side clustering

--- server/index.js
+++ server/index.js
@@ -3,17 +3,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const cluster = require('cluster');
 const router = require('./router.js');
 
-const port = process.env.PORT || 3003;
+const masterProcess = () => {
+  const numCPUs = require('os').cpus().length;
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+};
 
-const app = express();
+const childProcess = () => {
+  const port = process.env.PORT || 3003;
+  const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(router);
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(router);
 
-app.use('/:id', express.static(path.join(__dirname, '../public')));
+  app.use('/:id', express.static(path.join(__dirname, '../public')));
 
-app.listen(port, () => console.log('Listening on port 3003'));
+  app.listen(port, () => console.log('Listening on port 3003'));
+};
+
+if (cluster.isMaster) {
+  masterProcess();
+} else {
+  childProcess();
+}
