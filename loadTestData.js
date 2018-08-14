03b38d894e79c3931ca2d9f7aa9308ed86d96f98
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-13 18:14:07 -0700

    Generate test data for patch requests

--- loadTestData.js
+++ loadTestData.js
@@ -7,11 +7,44 @@ const getRandomNumberWeightedTowardMax = (max) => {
   return 0.9 * max + Math.ceil(Math.random() * 0.1 * max);
 };
 
+const ltrs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+const generateName = (index, length) => {
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += ltrs[index % 10];
+    index = Math.floor(index / 10);
+  }
+  return result;
+};
+
 const generateWeightedId = (context, events, done) => {
   context.vars.id = getRandomNumberWeightedTowardMax(10000000);
   return done();
 };
 
+const generateWeightedName = (context, events, done) => {
+  const id = getRandomNumberWeightedTowardMax(10000000);
+  context.vars.name = generateName(id, 7);
+  return done();
+};
+
+const generateProductInfo = (context, events, done) => {
+  context.vars.id = getRandomNumberWeightedTowardMax(10000000);
+  context.vars.brand = 'brand';
+  context.vars.is_prime = 1;
+  context.vars.num_questions = 50;
+  context.vars.product_name = generateName(id, 4);
+  context.vars.product_price = 0.99;
+  context.vars.product_tier = 'first';
+  context.vars.review_totals = [5, 5, 5, 5, 5];
+  context.vars.seller_name = 'ben';
+  context.vars.stock_count = 1;
+  context.vars.thumbnail_url = 'url';
+  return done();
+};
+
 module.exports = {
   generateWeightedId,
+  generateWeightedName,
+  generateProductInfo,
 };
