76677e96c57186b3ebdee9c5256dab41417d8daa
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-15 10:13:17 -0700

    Modify load tests

--- loadTests/getTests.js
+++ loadTests/getTests.js
@@ -1,18 +1,21 @@
 const siege = require('siege');
 const { generateWeightedId, generateWeightedName } = require('./testData.js');
 
-let sieger = siege()
-  .concurrent(40)
-  .on(3003);
+let sieger = siege().on(3003);
 
-// GET product by id
+// GET product page
 // for (let i = 0; i < 100000; i++) {
-//   sieger = sieger.for(1).times.get(`/products/${generateWeightedId()}`);
+//   sieger = sieger.for(1).times.get(`/${generateWeightedId()}`);
 // }
 
-// GET product by name
+// GET product by id
 for (let i = 0; i < 100000; i++) {
-  sieger = sieger.for(1).times.get(`/products/name/${generateWeightedName()}`);
+  sieger = sieger.for(1).times.get(`/products/${generateWeightedId()}`);
 }
+
+// GET product by name
+// for (let i = 0; i < 100000; i++) {
+//   sieger = sieger.for(1).times.get(`/products/name/${generateWeightedName()}`);
+// }
 
 sieger.attack();
