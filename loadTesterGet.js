cc8d20e8d87f963cafa5f46d1e96b8089318b87c
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-14 17:35:12 -0700

    Clarify names of load test files

--- loadTesterGet.js
+++ loadTesterGet.js
@@ -8,13 +8,13 @@ const {
 let sieger = siege().on(3003);
 
 // GET product by id
-// for (let i = 0; i < 100000; i++) {
-//   sieger = sieger.for(1).times.get(`/products/${generateWeightedId()}`);
-// }
+for (let i = 0; i < 10000000; i++) {
+  sieger = sieger.for(1).times.get(`/products/${generateWeightedId()}`);
+}
 
 // GET product by name
-for (let i = 0; i < 100000; i++) {
-  sieger = sieger.for(1).times.get(`/products/name/${generateWeightedName()}`);
-}
+// for (let i = 0; i < 100000; i++) {
+//   sieger = sieger.for(1).times.get(`/products/name/${generateWeightedName()}`);
+// }
 
 sieger.attack();
