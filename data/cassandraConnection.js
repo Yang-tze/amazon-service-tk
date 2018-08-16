42e49452d45dc770e6259c03a81a0243e378984b
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-16 10:59:22 -0700

    Fix assignment of contactPoint for cassandra

--- data/cassandraConnection.js
+++ data/cassandraConnection.js
@@ -1,7 +1,7 @@
 const cassandra = require('cassandra-driver');
 
 const options = {
-  contactPoints: [process.env.CASSANDRA_HOST] || ['127.0.0.1'],
+  contactPoints: process.env.CASSANDRA_HOST ? [process.env.CASSANDRA_HOST] : ['127.0.0.1'],
   keyspace: 'product_db',
 };
 
