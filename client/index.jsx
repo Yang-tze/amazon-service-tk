ea25779e1dadfa8665710b2a789b66565142c1b6
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-17 11:14:14 -0700

    Refactor client to make react component an object on the window

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import ProductInfo from './components/ProductInfo.jsx';
-import ShippingInfo from './components/ShippingInfo.jsx';
+// import ShippingInfo from './components/ShippingInfo.jsx';
+
+window.ProductInfo = ProductInfo;
 
 ReactDOM.render(<ProductInfo />, document.getElementById('product-info'));
-ReactDOM.render(<ShippingInfo />, document.getElementById('shipping-info'));
